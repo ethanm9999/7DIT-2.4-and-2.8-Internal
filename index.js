e1fd7b0d749576d37f0fc97ffa6d32f27ec66e2e
ethanm9999 <50854937+ethanm9999@users.noreply.github.com>
2021-08-17 20:34:36 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function updateClock(){
   var now = new Date();
   var dname = now.getDay(),
@@ -18,18 +17,3 @@ function initClock(){
   updateClock();
   window.setInterval("updateClock()", 1);
 }
-=======
-function formatDate(dateObject) {
-  const parts = {
-    date: dateObject.getDate(),
-    month: dateObject.getMonth() + 1,
-    year: dateObject.getFullYear(),
-  };
-
-  return `${parts.date}/${parts.month}/${parts.year}`
-}
-
-const dayJsObject = dayjs();
-
-console.log(dayJsObject.format("D/M/YYYY"));
->>>>>>> da72432eb811abf6b78cf6f92cbd6c505072c452
